8cc31e326e8ef47b3fc9a58d4a3c3d6d4449509a
caoduanxi <cao236476>
2019-08-03 13:56:41 +0800

    构建服务代码

---
+++ service-feign/src/main/java/com/duanxi/servicefeign/SchedualServiceHi.java
@@ -0,0 +1,16 @@
+package com.duanxi.servicefeign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 服务的生成者在注册中心中注册的名字
+ */
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
